c2bcf39493420913d07aaec7bf368dcd283406a5
mejrakucevic <mejrakucevic420@gmail.com>
2022-08-29 02:18:30 +0200

    .

--- app2.js
+++ app2.js
@@ -10,3 +10,27 @@ BigInt(420000000000000000000000000000000000000000);
 null;
 undefined;
 symbol("pretty");
+
+// Neprimitivni su za promenjljive sa vise vrednosti
+
+Array['Mejra', '17', true];
+Object {
+    ime: "Mejra",
+    godine: "17"
+}
+
+new Object ({ime: 'mejraa'})
+
+new Set(["jabuka","kruska"]);
+new Map([
+    ["apples"],
+    ["bananas"]
+]);
+
+var x; // deklaracija
+x = 10; // inicijalizacija
+var x = 10; // deklaracija i inicijalizacija
+
+let broj1 // deklaracija
+let broj1 = 17 // inicijalizacija
+

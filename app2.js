06b116465383ab1ac4a1df52c97db81ee006b0b2
mejrakucevic <mejrakucevic420@gmail.com>
2022-08-29 02:10:43 +0200

    .

--- app2.js
+++ app2.js
@@ -1,1 +1,12 @@
 document.getElementById("vezbanje").innerHTML="Ovo je priprema za test. ";
+
+// Primitivni i Neprimitivni tipovi podataka.
+// Primitivni se koriste za promenjljive sa jednom vrednoscu
+
+string("Reyna best agent");
+Number(18);
+Boolean("uslov");
+BigInt(420000000000000000000000000000000000000000);
+null;
+undefined;
+symbol("pretty");

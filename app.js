4818cfd9a344873118c17230eacd547e5f51bea5
mejrakucevic <mejrakucevic420@gmail.com>
2022-09-12 18:33:44 +0200

    .

---
+++ app.js
@@ -0,0 +1,17 @@
+// const Books = {
+//     title: "Before The Coffee Gets Cold",
+//     author: "Toshikazu Kawaguchi",
+//     pages: "272",
+//     read: "Yes",
+//     info : [title, author, pages, read]
+// };
+
+
+// console.log(Books.info());
+
+function books(title, author, pages, read) {
+    this.title = "Before";
+    this.author = "Toshikazu";
+    this.pages = "222";
+  }
+
